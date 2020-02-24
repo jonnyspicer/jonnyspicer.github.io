adbfa99cd83e5f157a743deed8dd1942e11c887d
Jonny <spicerjonny@gmail.com>
2020-02-24 13:10:51 +0000

    wordcount script now strips html tags using beautifulsoup

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,5 @@
 import os
+from bs4 import BeautifulSoup
 
 directory = input('Which directory would you like a word count for?')
 wordcount = 0
@@ -8,7 +9,8 @@ for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".md") or filename.endswith(".html"):
         with open(directory + '/' + filename, 'r') as file:
-            text = file.read().replace('\n', '').split('---', 2)
+            text = BeautifulSoup(file.read().replace(
+                '\n', ''), features="html.parser").get_text().split('---', 2)
             wordcount += len(text[2].split())
             for words in text[2].lower().split():
                 uniquewords.add(words)
