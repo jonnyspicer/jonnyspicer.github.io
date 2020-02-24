bcfcee1e8e0c137307b90255b9c0bb6e5d7974aa
Jonny <spicerjonny@gmail.com>
2020-02-24 11:56:21 +0000

    update wordcount script to get number of unique words

--- wordcount.py
+++ wordcount.py
@@ -2,6 +2,7 @@ import os
 
 directory = input('Which directory would you like a word count for?')
 wordcount = 0
+uniquewords = set([])
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
@@ -9,8 +10,11 @@ for file in os.listdir(directory):
         with open(directory + '/' + filename, 'r') as file:
             text = file.read().replace('\n', '').split('---', 2)
             wordcount += len(text[2].split())
+            for words in text[2].lower().split():
+                uniquewords.add(words)
         continue
     else:
         print('That directory doesn\'t appear to have any posts in!')
 
-print(wordcount)
+print('Wordcount: ' + str(wordcount))
+print('Unique words: ' + str(len(uniquewords)))
