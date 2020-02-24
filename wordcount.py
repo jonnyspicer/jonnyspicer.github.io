441c7d07b988ed77428c324985646b5df99202e0
Jonny <spicerjonny@gmail.com>
2020-02-24 13:21:26 +0000

    add comment to wordcount script so it's a bit more penetrable

--- wordcount.py
+++ wordcount.py
@@ -9,6 +9,9 @@ for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".md") or filename.endswith(".html"):
         with open(directory + '/' + filename, 'r') as file:
+            # This is a bit of a mouthful... it takes the raw text, uses BS4 to strip out
+            # html tags, replaces line endings to it's on a single line, then selects
+            # everything after the Jekyll front matter
             text = BeautifulSoup(file.read().replace(
                 '\n', ''), features="html.parser").get_text().split('---', 2)
             wordcount += len(text[2].split())
