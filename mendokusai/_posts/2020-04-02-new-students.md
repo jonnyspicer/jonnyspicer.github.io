a5714350eccfe71c1a2cec89e98f0c581ce24672
Jonny Spicer <spicerjonny@gmail.com>
2020-04-02 20:06:46 +0100

    dp200402

---
+++ mendokusai/_posts/2020-04-02-new-students.md
@@ -0,0 +1,14 @@
+---
+layout: mendokusai
+author: Jonny Spicer
+title: New Students
+tags: [ Chess ]
+---
+The whole world is looking for new indoor activities with which to amuse themselves, and it seems a lot of people are
+turning to chess - just take a quick look at chess.com's [server load](https://www.chess.com/blog/erik/chess-com-during-this-global-stay-home-moment){:target="_blank"}
+if you don't believe me. It seems some of these new students are looking for masters and a couple of my friends have
+even reached out to me for lessons (not that I am a master, especially not so in the strict chess sense).
+
+Given that I'm used to playing almost exclusively with players > 2x my age, it'll be quite nice to be able to talk
+about the game with people who are in the same place in life as me, and it'll be doubly good to have my ego stroked
+as I do my best to impart unto them what little chess wisdom I've gleaned over the years.
--- publish.py
+++ publish.py
@@ -20,7 +20,7 @@ else:
 add = subprocess.Popen(["git", "add", "-A"])
 stdoutput, stderroutput = add.communicate()
 
-commit = subprocess.Popen(["git", "commit", "-m", commitMessage], shell=True, stdout=PIPE, stderr=PIPE)
+commit = subprocess.Popen(["git", "commit", "-m", commitMessage])
 stdoutput, stderroutput = commit.communicate()
 
 if b'fatal' in stdoutput:
@@ -37,34 +37,5 @@ if b'fatal' in stdoutput:
     sys.exit()
 else:
     print("Push successful")
-
-print("Waiting for Github Pages to build...")
-
-time.sleep(30)
-
-def LastPostDate():
-    frontpage = urlopen('https://jonnyspicer.com').read()
-    parsed_frontpage = BeautifulSoup(frontpage, 'html.parser')
-    date_span = parsed_frontpage.body.find(
-        'div', class_='recent-posts-mendokusai').find('span').text
-    date_today = now.strftime("%d %b %Y")
-    return date_today in date_span
-
-print("Checking whether new post is on live site")
-print("First try...")
-if LastPostDate() == True:
-    print("Post published!")
-else:
-    time.sleep(30)
-    print("Second try...")
-    if LastPostDate() == True:
-        print("Post published!")
-    else:
-        time.sleep(30)
-        print("Third try...")
-        if LastPostDate() == True:
-            print("Post published!")
-        else:
-            print("Something went wrong. If only you'd written an actual unit test with exceptions!")
 
 
